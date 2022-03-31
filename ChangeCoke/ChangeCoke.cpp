1beb02a9961e9ed8a3979857ac1cf0882151e4fa
xuke105 <xuke105@jd.com>
2022-03-31 09:41:47 +0800

    添加    某商店规定：三个空汽水瓶可以换一瓶汽水，允许向老板借空汽水瓶（但是必须要归还）。 小张手上有n个空汽水瓶，她想知道自己最多可以喝到多少瓶汽水。 数据范围：输入的正整数满足
    注意：本题存在多组输入。输入的 0 表示输入结束，并不用输出结果。

---
+++ ChangeCoke/ChangeCoke.cpp
@@ -0,0 +1,50 @@
+﻿/*某商店规定：三个空汽水瓶可以换一瓶汽水，允许向老板借空汽水瓶（但是必须要归还）。
+小张手上有n个空汽水瓶，她想知道自己最多可以喝到多少瓶汽水。
+数据范围：输入的正整数满足  1<=n<=100
+
+注意：本题存在多组输入。输入的 0 表示输入结束，并不用输出结果。
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int iNum;
+	char strNum[5];
+	while (true)
+	{
+		cin.getline(strNum, 5);
+		iNum = atoi(strNum);
+		if (iNum<=1)
+		{
+			iNum = 1;
+		}
+		else if (iNum>=100)
+		{
+			iNum = 100;
+		}
+
+		int iNewN = 0;
+		while (iNum >= 2)
+		{
+			if (2 == iNum)
+			{
+				iNewN += 1;
+				break;
+			}
+			else if (iNum < 2)
+			{
+				break;
+			}
+			else
+			{
+				int iNewTemp = (iNum / 3);
+				iNewN += iNewTemp;
+				iNum %= 3;
+				iNum += iNewTemp;
+			}
+		}
+		std::cout << iNewN << std::endl;
+	}
+	return 0;
+}
