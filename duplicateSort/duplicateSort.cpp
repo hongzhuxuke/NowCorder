69833ab8f331d1cff23dfb33c71fe49c75c1b121
xuke105 <xuke105@jd.com>
2022-03-31 11:02:07 +0800

    明明生成了N个1到500之间的随机整数。请你删去其中重复的数字，即相同的数字只保留一个，把其余相同的数去掉，然后再把这些数从小到大排序，按照排好的顺序输出。
    数据范围：  1<=n<=100，输入的数字大小满足    输入描述:
    第一行先输入随机整数的个数 N 。
    接下来的 N 行每行输入一个整数，代表明明生成的随机数。
    具体格式可以参考下面的"示例"。
    
    输出描述:
    输出多行，表示输入数据处理后的结果

---
+++ duplicateSort/duplicateSort.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+using namespace std;
+void CountSort(int iArray[], int iStart, int iLen)
+{
+	int *iArrayTemp = new int[100]{ 0 };
+	int bitNum = 100;
+	//此处第一次并未考虑100 以内的数据
+	if(iArray[iStart]>100)
+		bitNum  = iArray[iStart] / 100 * 100;
+
+	for (int i = iStart; i < iLen; i++)
+	{
+		int iTemp = iArray[i] % bitNum;
+		iArrayTemp[iTemp] = iArray[i];
+	}
+	for (int i = iStart; i < iLen; i++)
+	{
+		iArray[i] = iArrayTemp[i];
+	}
+	delete[]iArrayTemp;
+}
+void buckSort(int iArray[], int iLen)
+{
+	int* iTempArray = new int[iLen] {0};
+	int *iCount = new int[6]{ 0 };
+	for (int i = 0; i < iLen; i++)
+	{
+		int iTemp = iArray[i] / 100;
+		iTemp %= 10;
+		iCount[iTemp]++;
+	}
+
+	for (int i = 1; i < 6; i++)
+	{
+		iCount[i] += iCount[i - 1];
+	}
+
+	int iCur = 0;
+	for (int i = 0; i < iLen; i++)
+	{
+		int iTemp = iArray[i] / 100;
+		iTemp %= 10;
+		if (iTemp > 0)
+		{
+			iTempArray[iCount[iTemp - 1]++] = iArray[i];
+		}
+		else
+		{
+			iTempArray[iCur++] = iArray[i];
+		}
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		iCount[i] = 0;
+	}
+
+	for (int i = 0; i < iLen; i++)
+	{
+		int iTemp = iArray[i] / 100;
+		iTemp %= 10;
+		iCount[iTemp]++;
+	}
+
+	for (int i = 1; i < 6; i++)
+	{
+		iCount[i] += iCount[i - 1];
+	}
+	for (int i = 0; i < iLen; i++)
+	{
+		iArray[i] = iTempArray[i];
+	}
+
+	CountSort(iArray, 0, iCount[0]);
+	for (int i = 0; i < 4; i++)
+	{
+		CountSort(iArray, iCount[i], iCount[i + 1]);
+	}
+	delete[]iCount;
+	delete[]iTempArray;
+}
+
+int main()
+{
+	int iN = 0;
+	scanf_s("%d", &iN);
+	int i = 0;
+	int *iNumArray = new int[iN];
+	while (i < iN)
+	{
+		scanf_s("%d", &iNumArray[i]);
+		i++;
+	}
+	buckSort(iNumArray, iN);
+
+	for (int i = 0; i < iN; i++)
+	{
+		if (iNumArray[i] > 0)
+		{
+			printf("%d\n", iNumArray[i]);
+		}
+	}
+
+	delete[]iNumArray;
+	return 0;
+}
