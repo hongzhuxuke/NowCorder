1bbd89b63756cc02cc676c2f2abad5d292e053df
xuke105 <xuke105@jd.com>
2022-04-02 09:21:48 +0800

    给定一个整数数组nums和一个目标值taeget，计算出和为目标值的两个整数的下标数组；

---
+++ CalTargeFromArray/CalTargeFromArray.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+#include <vector>
+//#include <array>
+#include <unordered_map>
+
+using namespace std;
+vector<int> CalTarge(int iArray[], int iMaxN, int iTarge);
+
+int main()
+{
+	int iArray[] = {1,3,4,2,5,1,7,3,7};
+	vector<int> vec = CalTarge(iArray, sizeof(iArray)/sizeof(int), 10);
+	
+	for (int i = 0; i< vec.size(); i++)
+	{
+		std::cout << vec[i] << " ";
+	}
+	std::cout << endl;
+	return 0;
+}
+
+std::vector<int> CalTarge(int iArray[], int iMaxN, int iTarge)
+{
+	vector<int> iRef;
+	unordered_map<int, int> Result;
+	for (int i = 0; i< iMaxN; i++)
+	{
+		if (iArray[i]< iTarge)
+		{
+			unordered_map<int, int>::iterator it = Result.find(iTarge - iArray[i]);
+			if (it!= Result.end())
+			{
+				iRef.push_back(it->second);
+				iRef.push_back(i);
+				return iRef;
+			}
+			Result[iArray[i]] =  i;
+			//Result.insert(std::pair<const_cast<int>( iArray[i]), i>);
+			//Result.insert(unordered_map<int,int>.value_type(  iArray[i], i));
+		}
+
+	}
+
+	return iRef;
+}
