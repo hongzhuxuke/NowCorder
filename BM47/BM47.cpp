7882b2331e9cc3a1de0042d9d01700b546e25167
xuke105 <xuke105@jd.com>
2022-04-09 20:49:29 +0800

    BM49 表达式求值 描述 请写一个整数计算器，支持加减乘三种运算和括号。
    数据范围：0\le |s| \le 1000≤∣s∣≤100，保证计算结果始终在整型范围内
    
    要求：空间复杂度： O(n)O(n)，时间复杂度 O(n)O(n)
    示例1
    输入：
    "1+2"
    复制
    返回值：
    3
    复制
    示例2
    输入：
    "(2*(3-4))*5"
    复制
    返回值：
    -10
    复制
    示例3
    输入：
    "3+2*3*4-1"
    复制
    返回值：
    26

--- BM47/BM47.cpp
+++ BM47/BM47.cpp
@@ -5,104 +5,112 @@
 #include <vector>
 using namespace std;
 
+class Solution {
+public:
+	int findKth(vector<int> a, int n, int K) {
+		// write code here
+		quicksort(a, 0, n - 1, K);
+		return a[K - 1];
+	}
+private:
+
+	int quicksort(vector<int>&a, int s, int e, int k)
+	{
+		while (true)
+		{
+			int i = s;
+			int j = e;
+
+			int index = i + rand() % (j - i + 1);
+			swap(a[index], a[e]);//随机挑选一个数字作为划分依据。以免出现复杂度过高的情况
+
+			int temp = a[s];
+
+			while (i < j)
+			{
+				while (i < j&& a[j] <= temp)
+				{
+					j--;
+				}
+				a[i] = a[j];
+				while (i < j && a[i] >= temp)
+				{
+					i++;
+				}
+				a[j] = a[i];
+			}
+
+			a[j] = temp;
+
+			if (k == j + 1) return a[j];
+			else if (j + 1 > k)
+			{
+				e = j - 1;
+				//return quicksort(a, s, j - 1, k); 
+			}
+			else
+			{
+				s = j + 1;
+				//quicksort(a, j + 1, e, k);
+			}
+		}
+
+	}
+};
+//
 //class Solution {
 //public:
 //	int findKth(vector<int> a, int n, int K) {
 //		// write code here
 //		quicksort(a, 0, n - 1, K);
-//		return a[n - K];
+//		return a[K-1];
 //	}
 //private:
-//	void swap(vector<int>&a, int i, int j)
-//	{
-//		if (i >= 0 && i < a.size() && j >= 0 && j < a.size() && i != j)
-//		{
-//			int t = a[i];
-//			a[i] = a[j];
-//			a[j] = t;
-//		}
-//	}
-//
 //
 //	int quicksort(vector<int>&a, int s, int e, int k)
 //	{
-//		if (s < 0 || s >= e) return a[0];
 //		int i = s;
-//		int j = e + 1;
-//		while (i <= j)
+//		int j = e;
+//		int temp = a[s];
+//		while (i < j)
 //		{
-//			while (a[++i] < a[s]) if (i == e) break;
-//			while (a[--j] > a[s]) if (j == s) break;
-//			if (i >= j) break;
-//			swap(a, i, j);
+//			while (i < j&& a[j] <= temp)
+//			{
+//				j--;
+//			}
+//			a[i] = a[j];
+//			while (i < j && a[i] >= temp)
+//			{
+//				i++;
+//			}
+//			a[j] = a[i];
 //		}
 //
-//		swap(a, s, j);
-//		if (a.size() - k == j) return 0;
-//		else if (j < a.size() - k) return quicksort(a, j + 1, e, k);
-//		else return quicksort(a, s, j - 1, k);
+//		a[j] = temp;
 //
-//		return 0;
+//		if (k == j + 1) return a[j];
+//		else if (j + 1 > k) return quicksort(a, s, j - 1, k); 
+//		else return quicksort(a, j + 1, e, k);
 //	}
 //};
-
-
-class Solution {
-public:
-	int quickSort(vector<int> a, int s, int e, int K)
-	{
-		int t = a[s];
-		int l = s+1;//左侧指针的开始位置  s+1即可
-		int r = e;
-		
-		while (l < r)
-		{
-			while (l<=e && a[l] >= t)//前半部分排序的是大于等于的，数组越界考虑
-			{
-				l++;
-			}
-			if (l > r) break;
-			while (r>s && a[r] < t)//数组越界考虑
-			{
-				r--;
-			}
-			if (l > r) break;
-			swap(a[l], a[r]);
-		}
-		a[s] = a[r];
-		a[r] = t;
-
-		if (r == K - 1) return a[r];
-		else if (r < K - 1)
-		{
-			return quickSort(a, r + 1, e, K);
-		}
-		else return quickSort(a, s, r - 1, K);
-	}
-
-	int findKth(vector<int> a, int n, int K) {
-		return quickSort(a, 0, n - 1, K);
-	}
-};
 
 int main()
 {
 	Solution  sl;
 	vector<int> iV;
 
-	iV.push_back(6);
-	iV.push_back(6);
-	iV.push_back(6);
+	iV.push_back(10);
+	iV.push_back(10);
+	iV.push_back(9);
+	iV.push_back(9);
 	iV.push_back(8);
-	iV.push_back(9);
-	iV.push_back(9);
-	iV.push_back(9);
-	iV.push_back(12);
-	iV.push_back(12);
-	iV.push_back(12);
-	iV.push_back(14);
-	int a = sl.findKth(iV, 94875, 18674);
+	iV.push_back(7);
+	iV.push_back(5);
+	iV.push_back(6);
+	iV.push_back(4);
+	iV.push_back(3);
+	iV.push_back(4);
+	iV.push_back(2);
+	int a = sl.findKth(iV, 12, 3);
 	a = 0;
 }
-
-
---
+++ BM49/BM49.cpp
@@ -0,0 +1,119 @@
+﻿/*
+BM49 表达式求值
+描述
+请写一个整数计算器，支持加减乘三种运算和括号。
+
+数据范围：0\le |s| \le 1000≤∣s∣≤100，保证计算结果始终在整型范围内
+
+要求：空间复杂度： O(n)O(n)，时间复杂度 O(n)O(n)
+示例1
+输入：
+"1+2"
+复制
+返回值：
+3
+复制
+示例2
+输入：
+"(2*(3-4))*5"
+复制
+返回值：
+-10
+复制
+示例3
+输入：
+"3+2*3*4-1"
+复制
+返回值：
+26
+
+*/
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+	/**
+	 * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+	 * 返回表达式的值
+	 * @param s string字符串 待计算的表达式
+	 * @return int整型
+	 */
+	int solve(string s) {
+		// write code here
+		return function(s);
+	}
+
+private:
+	void appStk(stack<int>& stk, int& iN, char temp[]) {
+		if (temp[0] == '-') {
+			stk.push(-1 * iN);
+		}
+		else if (temp[0] == '*') {
+			stk.top() *= iN;
+		}
+		else {
+			stk.push(iN);
+		}
+		iN = 0;
+	}
+
+	int function(string s) {
+		int iNum = 0;
+		stack<int> stk;
+		int tN = 0;
+		int i = 0;
+		char Cal[2]{ '\0' };
+		int iBrakets = 0;
+		string st = "";
+		char temp[2]{ '\0' };
+
+		while (i < s.length()) {
+			if (iBrakets > 0) {
+				if (s.at(i) == ')') {
+					--iBrakets;
+					if (0 == iBrakets) {
+						tN = function(st);
+						st = "";
+						continue;
+					}
+				}
+				else if (s.at(i) == '(') {
+					++iBrakets;
+				}
+				temp[0] = s.at(i);
+				st.append(temp);
+			}
+			else if (s.at(i) >= '0' && s.at(i) <= '9') {
+				tN *= 10;
+				tN += (s.at(i) - '0');
+			}
+			else if (s.at(i) == '(') {
+				//appStk(stk, tN, Cal);//括号前边不会是数字，不然   2*（）  会引起 2*0  等于0 了
+				st = "";
+				++iBrakets;
+			}
+			else if (s.at(i) == '+' || s.at(i) == '-' || s.at(i) == '*') {
+				appStk(stk, tN, Cal);
+				Cal[0] = s.at(i);
+			}
+			i++;
+		}
+		appStk(stk, tN, Cal);
+
+		while (!stk.empty()) {
+			iNum += stk.top();
+			stk.pop();
+		}
+		return iNum;
+	}
+
+
+};
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+}
