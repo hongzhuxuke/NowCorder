aa16e4924939356c4091f07e8ccea18e39bac9d0
xuke105 <xuke105@jd.com>
2022-04-12 11:31:34 +0800

    BM18 二维数组中的查找
    描述
    在一个二维数组array中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
    [
    [1,2,8,9],
    [2,4,9,12],
    [4,7,10,13],
    [6,8,11,15]
    ]
    给定 target = 7，返回 true。
    
    给定 target = 3，返回 false。
    
    数据范围：矩阵的长宽满足 0 \le n,m \le 5000≤n,m≤500 ， 矩阵中的值满足 0 \le val \le 10^90≤val≤10
    9
    
    进阶：空间复杂度 O(1)O(1) ，时间复杂度 O(n+m)O(n+m)
    示例1
    输入：
    7,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
    复制
    返回值：
    true
    复制
    说明：
    存在7，返回true
    示例2
    输入：
    1,[[2]]
    复制
    返回值：
    false
    复制
    示例3
    输入：
    3,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
    复制
    返回值：
    false
    复制
    说明：
    不存在3，返回false

--- BM49/BM49.cpp
+++ BM49/BM49.cpp
@@ -35,14 +35,8 @@ using namespace std;
 
 class Solution {
 public:
-	/**
-	 * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-	 * 返回表达式的值
-	 * @param s string字符串 待计算的表达式
-	 * @return int整型
-	 */
+
 	int solve(string s) {
-		// write code here
 		return function(s);
 	}
 
